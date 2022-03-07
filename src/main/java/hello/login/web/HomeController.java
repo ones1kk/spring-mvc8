3899e048a5e4da7f888d956a74df30106df4cbea
ones1kk <kwsic@naver.com>
2022-03-07 23:24:10 +0900

    ✨ Add : Session set time out(maxinactiveinterval)

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -4,13 +4,12 @@ import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
 import hello.login.web.session.SessionConst;
 import hello.login.web.session.SessionManager;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -56,7 +55,7 @@ public class HomeController {
         return "loginHome";
     }*/
 
-    @GetMapping
+   /* @GetMapping
     public String homeLoginV3(HttpServletRequest request, Model model) {
         HttpSession session = request.getSession(false);
         if (session == null) {
@@ -65,6 +64,19 @@ public class HomeController {
 
         Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
 
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+
+        return "loginHome";
+    }*/
+
+    @GetMapping
+    public String homeLoginV3Spring(
+        @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member,
+        Model model) {
         if (member == null) {
             return "home";
         }
