48008fdf3974e9f1b3aecd48016d0c3a200270cb
ones1kk <kwsic@naver.com>
2022-03-07 22:35:51 +0900

    ✨ Add : Login with sessionManager

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,11 +2,12 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
@@ -15,12 +16,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+
+    private final SessionManager sessionManager;
+
  /*   @GetMapping("/")
     public String home() {
         return "home";
     }*/
 
-    @GetMapping
+    /*@GetMapping
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId,
         Model model) {
         if (memberId == null) {
@@ -33,5 +37,20 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "loginHome";
 
+    }*/
+
+    @GetMapping
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+
+        return "loginHome";
     }
 }
