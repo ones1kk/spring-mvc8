ce819f497ce546c8b3f8bf1c60b2e8c9f49470b3
ones1kk <kwsic@naver.com>
2022-03-08 23:18:47 +0900

    ✨ Add : ArgumentResolver

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
@@ -73,10 +74,21 @@ public class HomeController {
         return "loginHome";
     }*/
 
-    @GetMapping
+    /*@GetMapping
     public String homeLoginV3Spring(
         @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member,
         Model model) {
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+
+        return "loginHome";
+    }*/
+
+    @GetMapping
+    public String homeLoginV3ArgumentResolver(@Login Member member, Model model) {
         if (member == null) {
             return "home";
         }
